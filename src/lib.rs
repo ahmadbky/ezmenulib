f1752839aaef3777b30a8994f429b926c3347328
Ahmad <ahmad.baa003@gmail.com>
2022-02-01 15:04:59 +0100

    added basic documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,43 @@
-//! This crate provides several tools to build a CLI menu easily.
-//! It also defines its Menu derive.
+//! Fast designing menus for your Rust CLI programs.
+//!
+//! This crate provides a `derive(Menu)` procedural macro to easily build menus.
+//! It includes type-checking from the user input, and a formatting customization.
+//!
+//! ## Example
+//!
+//! Here is an example of how to use it:
+//! ```rust
+//! use ezmenu::Menu;
+//!
+//! fn check(n: &i32) {
+//!     if *n == 0 {
+//!         println!("yo respect me plz :'(");
+//!     } else {
+//!         println!("good. :)");
+//!     }
+//! }
+//!
+//! #[derive(Menu)]
+//! struct MyMenu {
+//!     author: String,
+//!     #[field(msg = "Give a nonzero number", then(check))]
+//!     number: u32,
+//! }
+//! ```
+//!
+//! To display the menu, you instantiate the struct by calling its `from_menu` method:
+//! ```rust
+//! let MyMenu { author, number } = MyMenu::from_menu();
+//! println!("values provided: author={}, number={}", author, number);
+//! ```
+//!
+//! This sample code prints the standard menu like above:
+//! ```md
+//! - author: Ahmad
+//! - Give a nonzero number: 0
+//! yo respect me plz :'(
+//! values provided: author=Ahmad, number=0
+//! ```
 
 pub use ezmenu_derive::*;
 use std::io::{BufRead, Stdin, Stdout, Write};
