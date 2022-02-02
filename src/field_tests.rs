747fe753ccf7dc26e2b1c16e60668709c8aba7df
Ahmad <ahmad.baa003@gmail.com>
2022-02-02 23:43:10 +0100

    renamed tests.rs to field_tests.rs

---
+++ src/field_tests.rs
@@ -0,0 +1,31 @@
+use crate::Field;
+use std::io::Write;
+
+macro_rules! test_field {
+    ($ident:ident: $ty:ty, $input:literal) => {{
+        let mut input = $input.as_bytes();
+        let mut output = Vec::new();
+        let value: $ty = $ident.build(&mut input, &mut output);
+
+        String::from_utf8(output).unwrap()
+    }};
+}
+
+#[test]
+fn test_fields() {
+    // this should become the expanded version of a field
+    let lastname = Field::from("what is your last name?")
+        .then(|s: &String, w| {
+            if s.to_lowercase() == "baalbaky" {
+                writeln!(w, "are you his brother or sister?")
+                    .expect("Unable to write the message after providing the last name");
+            }
+        })
+        .display_default(false)
+        .chip(Some("Now, "))
+        .prefix(Some(">> "))
+        .new_line(true);
+
+    let output = test_field!(lastname: String, "baAlBaKy");
+    println!("{}", output);
+}
