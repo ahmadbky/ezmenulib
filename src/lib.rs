9ff3cc16bc7395506278f4936adadab1d081ee2f
Ahmad <ahmad.baa003@gmail.com>
2022-02-06 17:12:45 +0100

    defined new result and error types

--- src/lib.rs
+++ src/lib.rs
@@ -44,6 +44,36 @@ mod menu;
 
 /// Menu derive macro
 pub use ezmenu_derive::Menu;
+use std::error::Error;
 
 pub use field::{StructField, StructFieldFormatting};
 pub use menu::{Menu, StructMenu};
+
+use std::fmt::Debug;
+use std::io;
+
+/// The error type used by the menu builder.
+#[derive(Debug)]
+pub enum MenuError {
+    /// An IO error, when flushing, reading or writing values,
+    IOError(io::Error),
+    /// An incorrect type has been used as default.
+    IncorrectType(Box<dyn Debug>),
+    /// There is no more field to call for an output.
+    /// This error appears when calling `<StructMenu as Menu>::next` method whereas
+    /// the menu building has finished.
+    NoMoreField,
+    /// A custom error type.
+    /// You can define this type when mapping the output value of the `Menu::next_map` method,
+    /// by returning an `Err(MenuError::Custom(...))`
+    Custom(Box<dyn Error>),
+}
+
+impl From<io::Error> for MenuError {
+    fn from(e: io::Error) -> Self {
+        Self::IOError(e)
+    }
+}
+
+/// The main result type used in the EZMenu library.
+pub type MenuResult<T> = Result<T, MenuError>;
