160fbfbc8292c08f0bec107f41882ca5b0d8ecf7
Ahmad <ahmad.baa003@gmail.com>
2022-02-06 15:01:40 +0100

    testing stdin and stdout for menu

--- src/_main.rs
+++ src/_main.rs
@@ -1,18 +1,15 @@
-use ezmenu::Menu;
-
-fn play(_: &u8) {
-    println!("oui");
-}
-
-#[derive(Menu)]
-struct Person {
-    name: String,
-    #[field(msg("45"), then(play))]
-    age: u8,
-}
+use ezmenu::{Menu, StructField, StructMenu};
+use std::io::Write;
 
 // used as testing for now
 fn main() {
-    let Person { name, age } = Person::from_menu();
-    println!("name={}, age={}", name, age);
+    let mut menu = StructMenu::default()
+        .title("bonsoir")
+        .with_field(StructField::from("oui").default("43"))
+        .with_field(StructField::from("allo"));
+
+    let _first: u8 = menu.next();
+    let _second: String = menu.next_with(|_, w| {
+        w.write(b"nice name").expect("let me pat u");
+    });
 }
