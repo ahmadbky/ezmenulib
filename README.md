1f08ef564ce90afd7771e2bdb21dd6fcad93bea9
Ahmad <ahmad.baa003@gmail.com>
2022-02-13 15:32:35 +0100

    updated docs

--- README.md
+++ README.md
@@ -13,37 +13,57 @@ You can also check the [examples](examples) to learn with a practical way.
 ## Example
 
 Here is an example of how to use it:
-```rust
-use ezmenu::Menu;
 
-fn check(n: &i32) {
-    if *n == 0 {
-        println!("yo respect me plz :'(");
-    } else {
-        println!("good. :)");
+```rust
+use ezmenu::{Menu, MenuResult};
+
+fn map(val: u32, w: &mut impl Write) -> MenuResult<u32> {
+    if val == 1000 {
+        w.write(b"ok!\n")?;
     }
+    Ok(val)
 }
 
 #[derive(Menu)]
 struct MyMenu {
     author: String,
-    #[field(msg = "Give a nonzero number", then(check))]
+    #[field(msg = "Give a nonzero number", then(map))]
     number: u32,
 }
 ```
 
 To display the menu, you instantiate the struct by calling its `from_menu` method:
+
 ```rust
 let MyMenu { author, number } = MyMenu::from_menu();
 println!("values provided: author={}, number={}", author, number);
 ```
 
 This sample code prints the standard menu like above:
+
 ```
 - author: Ahmad
 - Give a nonzero number: 0
 yo respect me plz :'(
 values provided: author=Ahmad, number=0
+```
+
+### Custom I/O types
+
+If you are not using `std::io::Stdin` and `std::io::Stdout` types, you can provide your own
+types by enabling the `custom_io` feature in your Cargo.toml file:
+
+```toml
+[dependencies]
+ezmenu = { version = "0.2.0", features = ["custom_io"] }
+```
+
+Then you can instantiate your struct with:
+
+```rust
+use std::io::stdout;
+let input = b"Ahmad\n1000\n" as &[u8];
+let MyMenu { author, number } = MyMenu::from_io(input, stdout());
 ```
 
 ## WIP
--- src/lib.rs
+++ src/lib.rs
@@ -7,36 +7,56 @@
 //!
 //! Here is an example of how to use it:
 //! ```rust
-//! use ezmenu::Menu;
+//! use ezmenu::{Menu, MenuResult};
+//! use std::io::Write;
 //!
-//! fn check(n: &i32) {
-//!     if *n == 0 {
-//!         println!("yo respect me plz :'(");
-//!     } else {
-//!         println!("good. :)");
+//! fn map(val: u32, w: &mut impl Write) -> MenuResult<u32> {
+//!     if val == 1000 {
+//!         w.write(b"ok!\n")?;
 //!     }
+//!     Ok(val)
 //! }
 //!
 //! #[derive(Menu)]
 //! struct MyMenu {
 //!     author: String,
-//!     #[field(msg = "Give a nonzero number", then(check))]
+//!     #[menu(msg = "Give a nonzero number", then(map))]
 //!     number: u32,
 //! }
 //! ```
 //!
 //! To display the menu, you instantiate the struct by calling its `from_menu` method:
+//!
 //! ```rust
 //! let MyMenu { author, number } = MyMenu::from_menu();
 //! println!("values provided: author={}, number={}", author, number);
 //! ```
 //!
 //! This sample code prints the standard menu like above:
+//!
 //! ```md
 //! - author: Ahmad
 //! - Give a nonzero number: 0
 //! yo respect me plz :'(
 //! values provided: author=Ahmad, number=0
+//! ```
+//!
+//! ## Custom I/O types
+//!
+//! If you are not using `std::io::Stdin` or `std::io::Stdout` types, you can provide
+//! your types by enabling `custom_io` feature in your Cargo.toml file:
+//!
+//! ```toml
+//! [dependencies]
+//! ezmenu = { version = "0.2.0", features = ["custom_io"] }
+//! ```
+//!
+//! Then you can instantiate your struct with:
+//!
+//! ```
+//! use std::io::stdout;
+//! let input = b"Ahmad\n1000\n" as &[u8];
+//! let MyMenu { author, number } = MyMenu::from_io(input, stdout());
 //! ```
 
 mod field;
@@ -44,7 +64,6 @@ mod menu;
 
 /// Menu derive macro
 pub use ezmenu_derive::Menu;
-use std::error::Error;
 
 pub use field::{StructField, StructFieldFormatting};
 pub use menu::{Menu, StructMenu};
@@ -57,7 +76,7 @@ use std::io;
 pub enum MenuError {
     /// An IO error, when flushing, reading or writing values,
     IOError(io::Error),
-    /// An incorrect type has been used as default.
+    /// An incorrect type of value has been used as default value.
     IncorrectType(Box<dyn Debug>),
     /// There is no more field to call for an output.
     /// This error appears when calling `<StructMenu as Menu>::next` method whereas
@@ -66,7 +85,7 @@ pub enum MenuError {
     /// A custom error type.
     /// You can define this type when mapping the output value of the `Menu::next_map` method,
     /// by returning an `Err(MenuError::Custom(...))`
-    Custom(Box<dyn Error>),
+    Custom(Box<dyn Debug>),
 }
 
 impl From<io::Error> for MenuError {
