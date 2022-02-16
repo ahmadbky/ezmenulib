9d500bda6f3fa7cf276b5e9effcd27188c242fb6
Ahmad <ahmad.baa003@gmail.com>
2022-02-16 23:32:57 +0100

    better example

--- examples/mklicense/src/main.rs
+++ examples/mklicense/src/main.rs
@@ -9,12 +9,32 @@ enum Type {
 }
 
 #[derive(Menu)]
+#[menu(
+    title = "Describe the license you want to build:",
+    chip = "--> ",
+    prefix = ">> ",
+    new_line = true
+)]
 struct License {
+    #[menu(msg = "List of the authors")]
     authors: MenuVec<String>,
+    #[menu(msg = "The project name")]
     proj_name: String,
+    #[menu(msg = "The type of the license (MIT, GPL, BSD...)", default = "MIT")]
     ty: Type,
+    #[menu(msg = "Year of the project", default = 2022)]
+    year: u16,
 }
 
 fn main() {
-    let _l = License::from_menu_unwrap();
+    let License {
+        authors,
+        proj_name,
+        ty,
+        year,
+    } = License::from_menu_unwrap();
+    println!("authors: {:?}", authors);
+    println!("project name: {:?}", proj_name);
+    println!("type: {:?}", ty);
+    println!("year: {:?}", year);
 }
