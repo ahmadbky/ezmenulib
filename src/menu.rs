436245e8ddc3080a3363ef2f2536a80503507f27
Ahmad <ahmad.baa003@gmail.com>
2022-02-23 23:38:52 +0100

    fixes #2, updated menu docs

--- src/menu.rs
+++ src/menu.rs
@@ -1,68 +1,27 @@
 use crate::field::ValueFieldFormatting;
 use crate::{MenuError, MenuResult, ValueField};
-use std::array;
+use std::array::IntoIter;
 use std::fmt::Debug;
 use std::io::{stdin, stdout, Stdin, Stdout, Write};
 use std::rc::Rc;
 use std::str::FromStr;
 
-/// Represents a menu describing a struct.
+/// Represents a value-menu type, which means a menu that retrieves values from the user inputs.
 ///
-/// It has a global formatting applied to the fields it contains.
-///
-/// # Examples
-///
-/// For a make-licence CLI program for example, you can build the menu like above:
-/// ```
-/// use ezmenu::{ValueField, ValueFieldFormatting};
-/// let mut menu = StructMenu::default()
-///     .title("-- Mklicense --")
-///     .fmt(ValueFieldFormatting {
-///         chip: "* Give the ",
-///         ..Default::default()
-///     })
-///     .with_field(ValueField::from("project author name"))
-///     .with_field(ValueField::from("project name"))
-///     .with_field(ValueField::from("Give the year of the license")
-///         .default("2022")
-///         .fmt(ValueFieldFormatting {
-///             prefix: ">> ",
-///             new_line: true,
-///             ..Default::default()
-///         })
-///     );
-///
-/// let name: String = menu.next_map(|s: String, w| {
-///     if s.to_lowercase() == "ahmad" {
-///         w.write(b"Nice name!!")?;
-///     }
-///     Ok(s)
-/// }).unwrap();
-/// let proj_name: String = menu.next().unwrap();
-/// let proj_year: i64 = menu.next().unwrap();
-/// ```
-///
-/// The example below will display this menu:
-/// ```text
-/// -- Mklicense --
-/// * Give the project author name: ahmad
-/// Nice name!!
-/// * Give the project name: ezmenu
-/// * Give the year of the license (default: 2022)
-/// >> 2018
-/// ```
+/// The `N` const parameter represents the amount of [`ValueField`s](https://docs.rs/ezmenu/latest/ezmenu/struct.ValueField.html)
+/// It has a global formatting applied to the fields it contains by inheritance.
 pub struct ValueMenu<'a, const N: usize> {
     title: &'a str,
     fmt: Rc<ValueFieldFormatting<'a>>,
-    fields: array::IntoIter<ValueField<'a>, N>,
+    fields: IntoIter<ValueField<'a>, N>,
     reader: Stdin,
     writer: Stdout,
     // used to know when to print the title
     first_popped: bool,
 }
 
-/// The default menu uses `Stdin` as reader and `Stdout` as writer.
 impl<'a, const N: usize> From<[ValueField<'a>; N]> for ValueMenu<'a, N> {
+    /// Instantiate the value-menu from its value-fields array.
     fn from(fields: [ValueField<'a>; N]) -> Self {
         Self {
             fields: fields.into_iter(),
@@ -75,7 +34,6 @@ impl<'a, const N: usize> From<[ValueField<'a>; N]> for ValueMenu<'a, N> {
     }
 }
 
-/// Methods used to construct a menu describing a struct.
 impl<'a, const N: usize> ValueMenu<'a, N> {
     /// Give the global formatting applied to all the fields the menu contains.
     /// If a field has a custom formatting, it will uses the formatting rules of the field
@@ -109,8 +67,7 @@ where
 
     /// Returns the value mapped by the function specified as argument.
     ///
-    /// The function takes `(Output, &mut W)` as argument, where `Output` is the type of the output,
-    /// and `W` is the type of the writer (`Stdout` generally).
+    /// The function takes `(Output, &mut Stdout)` as argument, where `Output` is the type of the output.
     ///
     /// It returns a `MenuResult<Output>` to prevent from any error or return a custom error, with:
     /// `MenuError::Other(Box<dyn std::error::Debug>)`.
@@ -122,23 +79,24 @@ where
     }
 }
 
-impl<'a, const N: usize> AsRef<Stdout> for ValueMenu<'a, N> {
+impl<const N: usize> AsRef<Stdout> for ValueMenu<'_, N> {
     fn as_ref(&self) -> &Stdout {
         &self.writer
     }
 }
 
-impl<'a, const N: usize> AsMut<Stdout> for ValueMenu<'a, N> {
+impl<const N: usize> AsMut<Stdout> for ValueMenu<'_, N> {
     fn as_mut(&mut self) -> &mut Stdout {
         &mut self.writer
     }
 }
 
-impl<'a, Output, const N: usize> Menu<Output> for ValueMenu<'a, N>
+impl<Output, const N: usize> Menu<Output> for ValueMenu<'_, N>
 where
     Output: FromStr,
     Output::Err: 'static + Debug,
 {
+    /// Returns the output of the next field if present.
     fn next_output(&mut self) -> MenuResult<Output> {
         // prints the title
         if !self.first_popped {
