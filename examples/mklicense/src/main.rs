3f26e02c819e065a6d8c3c3242ad8e8523ba9781
Ahmad <ahmad.baa003@gmail.com>
2022-02-23 22:52:13 +0100

    updated tests

--- examples/mklicense/src/main.rs
+++ examples/mklicense/src/main.rs
@@ -3,12 +3,21 @@ use ezmenu::{Menu, MenuVec, ValueField, ValueFieldFormatting, ValueMenu};
 fn main() {
     let mut menu = ValueMenu::from([
         ValueField::from("Authors"),
-        ValueField::from("Project name"),
+        ValueField::from("Project name").fmt(ValueFieldFormatting {
+            chip: "--> ",
+            ..Default::default()
+        }),
         ValueField::from("Date").default("2022"),
     ])
     .fmt(ValueFieldFormatting {
         chip: "==> ",
         ..Default::default()
     });
-    let _authors: MenuVec<String> = menu.next_output().unwrap();
+    let _: MenuVec<String> = menu.next_output().unwrap();
+    let _: String = menu.next_output().unwrap();
+    let _: u16 = menu.next_output().unwrap();
+
+    let _: i32 = ValueField::from("Give the license type")
+        .init_build()
+        .unwrap();
 }
