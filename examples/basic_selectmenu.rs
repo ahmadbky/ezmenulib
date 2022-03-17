df0641d309f353a1ace29c276f6f1bfaa3be34f6
Ahmad <ahmad.baa003@gmail.com>
2022-03-17 20:04:10 +0100

    updated examples

--- examples/basic_selectmenu.rs
+++ examples/basic_selectmenu.rs
@@ -2,10 +2,10 @@ use ezmenulib::prelude::*;
 use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let amount: u8 = SelectMenu::from([
-        SelectField::from("0"),
-        SelectField::from("1"),
-        SelectField::from("2"),
+    let amount: i32 = SelectMenu::from([
+        SelectField::new("0", 0),
+        SelectField::new("1", 1),
+        SelectField::new("2", 2),
     ])
     .next_output()?;
 
--- examples/mklicense/src/main.rs
+++ examples/mklicense/src/main.rs
@@ -1,26 +1,12 @@
 use ezmenulib::customs::{MenuOption, MenuVec};
 use ezmenulib::prelude::*;
 use std::error::Error;
-use std::str::FromStr;
 
 #[derive(Debug)]
 enum Type {
     MIT,
     GPL,
     BSD,
-}
-
-impl FromStr for Type {
-    type Err = MenuError;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        match s.to_lowercase().as_str() {
-            "mit" => Ok(Self::MIT),
-            "gpl" => Ok(Self::GPL),
-            "bsd" => Ok(Self::BSD),
-            _ => Err(MenuError::from(format!("unknown license: {}", s))),
-        }
-    }
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -30,9 +16,9 @@ fn main() -> Result<(), Box<dyn Error>> {
         Field::Value(ValueField::from("License date").default_value("2022")),
         Field::Select(
             SelectMenu::from([
-                SelectField::from("MIT"),
-                SelectField::from("GPL"),
-                SelectField::from("BSD"),
+                SelectField::new("MIT", Type::MIT),
+                SelectField::new("GPL", Type::GPL),
+                SelectField::new("BSD", Type::BSD),
             ])
             .title(SelectTitle::from("Select a license type"))
             .default(0),
@@ -42,7 +28,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     let authors: MenuVec<String> = license.next_output()?;
     let name: MenuOption<String> = license.next_output()?;
     let date: u16 = license.next_output()?;
-    let ty: Type = license.next_output()?;
+    let ty: Type = license.next_select()?;
 
     println!(
         "{:?} License, Copyright (C) {} {}\n{}",
