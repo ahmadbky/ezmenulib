c6b077368d4ab0cf94e68ee3ae8a2c2c9438345c
Ahmad <ahmad.baa003@gmail.com>
2022-04-16 16:23:29 +0200

    testing formatting inheritance in examples

--- examples/retrieve_values.rs
+++ examples/retrieve_values.rs
@@ -3,8 +3,15 @@ use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let mut menu = Values::default();
-    let name: String = menu.written(&Written::from("name").example("Ahmad"))?;
-    let age: u8 = menu.written(&Written::from("age").format(&Format::show_default(false)))?;
+    let name: String = menu.written_or_default(&Written::from("name").example("Ahmad").format(&Format {
+        line_brk: false,
+        suffix: ": ",
+        ..Default::default()
+    }));
+    let age: u8 = menu.written_or_default(
+        &Written::from("age")
+            .format(&Format::show_default(false)),
+    );
     println!("ok you are {} y/o, goodbye {}!", age, name);
     Ok(())
 }
--- examples/selected_values.rs
+++ examples/selected_values.rs
@@ -3,6 +3,11 @@ use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let amount = Selected::new("how many", vec![("zero", 0), ("one", 1), ("two", 2)])
+        .format(&Format {
+            suffix: "> ",
+            line_brk: false,
+            ..Default::default()
+        })
         .default(1)
         .select(&mut MenuStream::default())?;
 
