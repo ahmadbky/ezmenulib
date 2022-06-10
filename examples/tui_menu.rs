f6fe35999f398b2dd8ae7675dbb8e6df66543930
Ahmad <ahmad.baa003@gmail.com>
2022-06-10 13:04:15 +0200

    set dependency backend features with `cursive`
    Set required features for `tui_menu` example.

--- examples/tui_menu.rs
+++ examples/tui_menu.rs
@@ -1,21 +1,31 @@
-use ezmenulib::{menu::tui_run::TuiMenu, prelude::*};
+use std::io::Stdout;
+
+use ezmenulib::{
+    prelude::*,
+    tui::{crossterm::{Crossterm, restore_terminal}, *},
+};
+use tui::{backend::CrosstermBackend, Terminal};
+
+fn first(term: &mut Terminal<CrosstermBackend<Stdout>>) -> MenuResult {
+    restore_terminal(term)?;
+    println!("you picked first");
+    Ok(())
+}
 
 fn main() -> MenuResult {
-    let mut menu = TuiMenu::try_from(&[
-        ("hello", TuiKind::Quit),
-        ("hey", TuiKind::Quit),
+    let mut menu = TuiMenu::<Crossterm>::try_from(&[
+        ("first", TuiKind::Map(first)),
+        ("second", TuiKind::Quit),
         (
-            "no",
+            "third",
             TuiKind::Parent(&[
-                ("first", TuiKind::Quit),
-                ("second", TuiKind::Quit),
+                ("fourth", TuiKind::Quit),
+                ("fifth", TuiKind::Quit),
                 ("go back", TuiKind::Back(1)),
             ]),
         ),
     ])?;
 
     menu.run()?;
-    menu.restore_term()?;
-
-    Ok(())
+    menu.close()
 }
